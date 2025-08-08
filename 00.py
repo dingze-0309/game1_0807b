d1cf55cbb73962e96133389c6fca98eed01084a9
leedingze <dingze0309@gmail.com>
2025-08-08 21:42:12 +0800

    更新說明

--- 00.py
+++ 00.py
@@ -1,6 +1,7 @@
 #初始化
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -15,6 +16,18 @@ def sixpartline():
     pygame.draw.line(screen, (128,0,0), (0,540), (1200,540), 10)
     pygame.display.flip()
 
+#方塊生成與下落
+class Block:
+    def __init__(self,x,y,color):
+        self.x = x
+        self.y = y
+        self.color = color
+    def draw(self,screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, 199, 20))
+    def move(self, speed):
+        self.y += speed
+
+
 #主程式
 running = True
 while running:
@@ -23,7 +36,7 @@ while running:
             running = False
 
     screen.fill((30, 30, 30))  # 背景填深灰色
-    #pygame.display.flip() 這行不要打，因為要等sixpartline畫完再一次刷新螢幕
+    #pygame.display.flip() 這行不要打，因為要等sixpartline執行完再一次刷新螢幕
     clock.tick(60)
 
     sixpartline()

83b83f92e6c5387c2a168e9dc0dcd225970c138a
leedingze <dingze0309@gmail.com>
2025-08-08 23:46:53 +0800

    08082345，part2方塊在上面六位置之一閃爍

--- 00.py
+++ 00.py
@@ -8,22 +8,25 @@ pygame.init()
 screen = pygame.display.set_mode((1200, 600))
 pygame.display.set_caption("My First Game")
 clock = pygame.time.Clock()
+blolist = []
+blosum = 0
 
 #畫六分線和判定線
 def sixpartline():
     for p in range(1, 6):
-        pygame.draw.line(screen, (255,255,255), (200*p,0), (200*p,600), 1)
-    pygame.draw.line(screen, (128,0,0), (0,540), (1200,540), 10)
+        pygame.draw.line(screen, (255, 255, 255), (200*p, 0), (200*p, 600), 1)
+    pygame.draw.line(screen, (128, 0, 0), (0, 540), (1200, 540), 10)
     pygame.display.flip()
 
 #方塊生成與下落
 class Block:
-    def __init__(self,x,y,color):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.color = color
-    def draw(self,screen):
+    def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, 199, 20))
+        pygame.display.flip()
     def move(self, speed):
         self.y += speed
 
@@ -40,6 +43,15 @@ while running:
     clock.tick(60)
 
     sixpartline()
+
+    #生成方塊
+    if random.randint(1, 600) <= 30:  # 5%的機率生成方塊
+        #blolist.append(blosum) 
+        block1 = Block(1 + random.randint(0, 5) * 200, 0, (70,250,200))
+        block1.draw(screen)
+        #blolist.append(block1)
+        #blosum += 1
+        Block
 
 pygame.quit()
 sys.exit()
